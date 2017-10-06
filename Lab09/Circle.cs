5d01d72861f890ae0f289db4235ec5f17affc4f1
Elijah <elijahdj88@gmail.com>
2017-10-06 16:25:39 -0400

    Calc circle circum and area first commit

---
+++ Lab09/Circle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09
+{
+    class Circle
+    {
+        private double radius;
+
+        public double Radius
+        {
+            set { radius = value; }
+            get { return radius; }
+        }
+
+        public double GetCircumference()
+        {
+            //C=2πr
+            double Circumference = 2*Math.PI*radius;
+            Console.WriteLine("Circumference: " + Circumference);
+            return Circumference;
+
+
+
+        }
+
+        public double GetArea()
+        {
+            //A=πr2
+            double Area = Math.PI * radius * radius;
+            Console.WriteLine("Area: " + Area);
+            return Area;
+        }
+
+       
+
+    }
+}
---
+++ Lab09/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepGoing = true;
+            while (keepGoing == true)
+            {
+                Circle CircleTester = new Circle();
+
+                Console.WriteLine("Welcome to the circle tester!");
+                Console.WriteLine();
+                Console.WriteLine("Enter radius:");
+                CircleTester.Radius = ReadValidNumbers();
+                if (CircleTester.Radius < 0)
+                {
+                    Console.WriteLine("Please enter valid number.");
+
+                }
+                else
+                {
+                    CircleTester.GetCircumference();
+                    CircleTester.GetArea();
+                }
+
+                string userChoice;
+                Console.WriteLine("Continue? (y/n)");
+                userChoice = Console.ReadLine();
+                if (userChoice == "n" || userChoice == "N")
+                {
+                    keepGoing = false;
+                }
+            }
+        }
+
+        public static double ReadValidNumbers()
+        {
+            double Input = 0;
+
+            while (!double.TryParse(Console.ReadLine(), out Input))
+            {
+
+                Console.WriteLine("Please enter a number!");
+            }
+            // after the while, you will have a valid number! 
+            return Input;
+
+
+
+        }
+    }
+}
